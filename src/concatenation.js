eb209351a3883017d8263ffc3e989e111c56cd34
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2023-11-26 18:25:45 +0200

    init

---
+++ src/concatenation.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.concatenation = void 0;
+function concatenation(firstWord, secondWord) {
+    console.log("".concat(firstWord, " ").concat(secondWord));
+    console.log(firstWord + 'test concat');
+}
+exports.concatenation = concatenation;
---
+++ src/concatenation.ts
@@ -0,0 +1,6 @@
+function concatenation(firstWord: string, secondWord: string) {
+    console.log(`${firstWord} ${secondWord}`);
+    console.log(firstWord+'test concat')
+}
+
+export { concatenation };
